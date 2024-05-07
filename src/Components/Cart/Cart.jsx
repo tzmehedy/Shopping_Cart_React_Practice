e5bccd69edc0bcd0955b2b05fee270094c42a5d6
unknown <mehedytouhidurzaman@gmail.com>
2024-05-07 21:34:35 +0600

    tailwind css added

---
+++ src/Components/Cart/Cart.jsx
@@ -0,0 +1,11 @@
+
+
+const Cart = () => {
+    return (
+        <div>
+            <h1>hello from cart</h1>
+        </div>
+    );
+};
+
+export default Cart;
---
+++ src/Components/Product/Product.jsx
@@ -0,0 +1,10 @@
+
+const Product = () => {
+    return (
+        <div>
+            <h1>Hello from product</h1>
+        </div>
+    );
+};
+
+export default Product;
